602b7f2c5d608f3d7ba9c387064a7058af66d0c1
zhangshuai2569 <zhangshuai950@163.com>
2019-02-18 23:55:39 +0800

    1.了解js与android端的交互，学习了webchromeclient()客户端，为js交互用的。 2.学习了webview加载本地assets文件夹下的HTML文件。 3.了解了js中 alert，prompt，confrim三种弹出框

--- mywebview/src/main/java/com/zhangshuai/mywebview/ActivityMain.java
+++ mywebview/src/main/java/com/zhangshuai/mywebview/ActivityMain.java
@@ -1,6 +1,11 @@
 package com.zhangshuai.mywebview;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Message;
 import android.support.annotation.Nullable;
@@ -13,10 +18,19 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.ClientCertRequest;
+import android.webkit.ConsoleMessage;
+import android.webkit.GeolocationPermissions;
 import android.webkit.HttpAuthHandler;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.PermissionRequest;
 import android.webkit.RenderProcessGoneDetail;
 import android.webkit.SafeBrowsingResponse;
 import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
+import android.webkit.WebBackForwardList;
+import android.webkit.WebChromeClient;
+import android.webkit.WebHistoryItem;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
@@ -49,12 +63,17 @@ public class ActivityMain extends AppCompatActivity {
 	/**
 	 * webview加载的网络地址
 	 */
-	private String DEFAULT_URL = "http://192.168.50.1:8080/";
+//	private String DEFAULT_URL = "http://192.168.50.1:8080/";
+	private String DEFAULT_URL = "file:////android_asset/sample.html";
 	/**
 	 * webview 设置类
 	 */
 	private WebSettings settings;
 	private String url;
+
+	private ProgressDialog progressDialog;
+	private WebBackForwardList webBackForwardList;
+	private WebHistoryItem itemAtIndex;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -76,7 +95,9 @@ public class ActivityMain extends AppCompatActivity {
 	 * @Date: 2019/2/17 21:58
 	 */
 
+	@SuppressLint("JavascriptInterface")
 	private void init() {
+		progressDialog=new ProgressDialog(this);
 		//		获取webview的设置封装类
 		settings = webview.getSettings();
 		//		启动js脚本的支持
@@ -114,6 +135,12 @@ public class ActivityMain extends AppCompatActivity {
 
 		//webview 客户端  【防止网页在手机浏览器中打开】
 		webview.setWebViewClient(new WebViewClient(){
+			/**
+			 * 当一个新的url即将加载到当前的WebView中时，让主机应用程序有机会接管控制权。
+			 * @param view
+			 * @param request
+			 * @return
+			 */
 			@Override
 			public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
 				Log.e(getClass().getName().toString(), "shouldOverrideUrlLoading"+url);
@@ -122,7 +149,7 @@ public class ActivityMain extends AppCompatActivity {
 					view.loadUrl(request.getUrl().toString());
 				}
 
-				return super.shouldOverrideUrlLoading(view, request);
+				return true;
 			}
 
 			/**
@@ -135,6 +162,8 @@ public class ActivityMain extends AppCompatActivity {
 			@Override
 			public void onPageStarted(WebView view, String url, Bitmap favicon) {
 				Log.e(getClass().getName().toString(), "onPageStarted");
+				progressDialog.setTitle("拼命加载中.....");
+				progressDialog.show();
 				super.onPageStarted(view, url, favicon);
 			}
 
@@ -146,6 +175,7 @@ public class ActivityMain extends AppCompatActivity {
 			@Override
 			public void onPageFinished(WebView view, String url) {
 				Log.e(getClass().getName().toString(), "onPageFinished");
+				progressDialog.dismiss();
 				super.onPageFinished(view, url);
 			}
 
@@ -160,24 +190,47 @@ public class ActivityMain extends AppCompatActivity {
 				super.onLoadResource(view, url);
 			}
 
+			/**
+			 * 通知主机应用程序将不再绘制从上一页导航遗留的WebView内容
+			 * @param view
+			 * @param url
+			 */
 			@Override
 			public void onPageCommitVisible(WebView view, String url) {
 				Log.e(getClass().getName().toString(), "onPageCommitVisible");
 				super.onPageCommitVisible(view, url);
 			}
 
+			/**
+			 * 向主机应用程序报告Web资源加载错误
+			 * @param view
+			 * @param request
+			 * @param error
+			 */
 			@Override
 			public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
 				Log.e(getClass().getName().toString(), "onReceivedError");
 				super.onReceivedError(view, request, error);
 			}
 
+			/**
+			 * 通知主机应用程序在加载资源时从服务器收到HTTP错误。
+			 * @param view
+			 * @param request
+			 * @param errorResponse
+			 */
 			@Override
 			public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
 				Log.e(getClass().getName().toString(), "onReceivedHttpError");
 				super.onReceivedHttpError(view, request, errorResponse);
 			}
 
+			/**
+			 * 作为主机应用程序，如果浏览器应该重新发送数据，因为请求的页面是POST的结果
+			 * @param view
+			 * @param dontResend
+			 * @param resend
+			 */
 			@Override
 			public void onFormResubmission(WebView view, Message dontResend, Message resend) {
 				Log.e(getClass().getName().toString(), "onFormResubmission");
@@ -196,21 +249,40 @@ public class ActivityMain extends AppCompatActivity {
 				super.doUpdateVisitedHistory(view, url, isReload);
 			}
 
+			/**
+			 * 当网页加载资源过程中发现SSL错误时回调
+			 * @param view
+			 * @param handler
+			 * @param error
+			 */
 			@Override
 			public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
 				Log.e(getClass().getName().toString(), "onReceivedSslError");
 				super.onReceivedSslError(view, handler, error);
 			}
 
+			/**
+			 * 通知主机应用程序来处理SSL客户端证书请求
+			 * @param view
+			 * @param request
+			 */
 			@Override
 			public void onReceivedClientCertRequest(WebView view, ClientCertRequest request) {
 				Log.e(getClass().getName().toString(), "onReceivedClientCertRequest");
 				super.onReceivedClientCertRequest(view, request);
 			}
 
+			/**
+			 * 通知应用程序WebView接收到了一个Http auth的请求
+			 * @param view
+			 * @param handler
+			 * @param host
+			 * @param realm
+			 */
 			@Override
 			public void onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler, String host, String realm) {
 				Log.e(getClass().getName().toString(), "onReceivedHttpAuthRequest");
+
 				super.onReceivedHttpAuthRequest(view, handler, host, realm);
 			}
 
@@ -220,6 +292,11 @@ public class ActivityMain extends AppCompatActivity {
 				return super.shouldOverrideKeyEvent(view, event);
 			}
 
+			/**
+			 * 通知主机应用程序，一个键未被WebView处理
+			 * @param view
+			 * @param event
+			 */
 			@Override
 			public void onUnhandledKeyEvent(WebView view, KeyEvent event) {
 				Log.e(getClass().getName().toString(), "onUnhandledKeyEvent");
@@ -238,12 +315,25 @@ public class ActivityMain extends AppCompatActivity {
 				super.onScaleChanged(view, oldScale, newScale);
 			}
 
+			/**
+			 * 通知应用程序有个自动登录帐号过程（通知主程序执行了自动登录请求）
+			 * @param view
+			 * @param realm
+			 * @param account
+			 * @param args
+			 */
 			@Override
 			public void onReceivedLoginRequest(WebView view, String realm, @Nullable String account, String args) {
 				Log.e(getClass().getName().toString(), "onReceivedLoginRequest");
 				super.onReceivedLoginRequest(view, realm, account, args);
 			}
 
+			/**
+			 * 通知主机应用程序，给定的Webview渲染进程已退出。
+			 * @param view
+			 * @param detail
+			 * @return
+			 */
 			@Override
 			public boolean onRenderProcessGone(WebView view, RenderProcessGoneDetail detail) {
 				Log.e(getClass().getName().toString(), "onRenderProcessGone");
@@ -256,18 +346,256 @@ public class ActivityMain extends AppCompatActivity {
 				super.onSafeBrowsingHit(view, request, threatType, callback);
 			}
 		});
+
+
+
+		/**webview 第二个客户端*/
+		webview.setWebChromeClient(new WebChromeClient(){
+
+			/**
+			 * 当前网页加载的进度
+			 * @param view
+			 * @param newProgress
+			 */
+			@Override
+			public void onProgressChanged(WebView view, int newProgress) {
+				super.onProgressChanged(view, newProgress);
+			}
+
+			/**
+			 * title变化的回调
+			 * @param view
+			 * @param title
+			 */
+			@Override
+			public void onReceivedTitle(WebView view, String title) {
+				super.onReceivedTitle(view, title);
+			}
+
+			/**
+			 * 图片变化的回调
+			 * @param view
+			 * @param icon
+			 */
+			@Override
+			public void onReceivedIcon(WebView view, Bitmap icon) {
+				super.onReceivedIcon(view, icon);
+			}
+
+			/**
+			 * 图片资源回调
+			 * @param view
+			 * @param url
+			 * @param precomposed
+			 */
+			@Override
+			public void onReceivedTouchIconUrl(WebView view, String url, boolean precomposed) {
+				super.onReceivedTouchIconUrl(view, url, precomposed);
+			}
+
+			@Override
+			public void onShowCustomView(View view, CustomViewCallback callback) {
+				super.onShowCustomView(view, callback);
+			}
+
+			@Override
+			public void onHideCustomView() {
+				super.onHideCustomView();
+			}
+
+			@Override
+			public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+				return super.onCreateWindow(view, isDialog, isUserGesture, resultMsg);
+			}
+
+			/**
+			 * webview请求得到focus，发生这个主要是当前webview不是前台状态，是后台webview
+			 * @param view
+			 */
+			@Override
+			public void onRequestFocus(WebView view) {
+				super.onRequestFocus(view);
+			}
+
+			@Override
+			public void onCloseWindow(WebView window) {
+				super.onCloseWindow(window);
+			}
+
+			/**
+			 * 覆盖默认的window.alert展示界面
+			 * @param view
+			 * @param url
+			 * @param message
+			 * @param result
+			 * @return
+			 */
+			@Override
+			public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+
+				Log.e("onJsAlert",message+"----"+result);
+
+				/*final AlertDialog.Builder builder = new AlertDialog.Builder(
+						view.getContext());
+
+				builder.setTitle("对话框").setMessage(message)
+						.setPositiveButton("确定", null);
+				builder.setOnKeyListener(new DialogInterface.OnKeyListener() {
+					public boolean onKey(DialogInterface dialog, int keyCode,
+										 KeyEvent event) {
+						Log.v("onJsAlert", "keyCode==" + keyCode + "event=" + event);
+						return true;
+					}
+				});
+				// 禁止响应按back键的事件
+				builder.setCancelable(false);
+				AlertDialog dialog = builder.create();
+				dialog.show();
+				result.confirm();// 因为没有绑定事件，需要强行confirm,否则页面会变黑显示不了内容。*/
+//				return true;
+
+
+
+				return super.onJsAlert(view, url, message, result);
+			}
+
+			/**
+			 * 覆盖默认的window.confirm展示界面(带确定和取消按钮的对话框)
+			 * @param view
+			 * @param url
+			 * @param message
+			 * @param result
+			 * @return
+			 */
+			@Override
+			public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+				Log.e("onJsConfirm",message+"----"+result);
+				return super.onJsConfirm(view, url, message, result);
+			}
+
+			/**
+			 * 覆盖默认的window.prompt展示界面(带输入框和选择按钮的对话框)
+			 * @param view
+			 * @param url
+			 * @param message
+			 * @param defaultValue
+			 * @param result
+			 * @return
+			 */
+			@Override
+			public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+				Log.e("onJsPrompt",message+"----"+result);
+				return super.onJsPrompt(view, url, message, defaultValue, result);
+			}
+
+			@Override
+			public boolean onJsBeforeUnload(WebView view, String url, String message, JsResult result) {
+				return super.onJsBeforeUnload(view, url, message, result);
+			}
+
+			@Override
+			public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+				super.onGeolocationPermissionsShowPrompt(origin, callback);
+			}
+
+			@Override
+			public void onGeolocationPermissionsHidePrompt() {
+				super.onGeolocationPermissionsHidePrompt();
+			}
+
+			@Override
+			public void onPermissionRequest(PermissionRequest request) {
+				super.onPermissionRequest(request);
+			}
+
+			@Override
+			public void onPermissionRequestCanceled(PermissionRequest request) {
+				super.onPermissionRequestCanceled(request);
+			}
+
+			/**
+			 * 处理了javascript的console的功能，js可以使用console打印调试信息
+			 * @param consoleMessage
+			 * @return
+			 */
+			@Override
+			public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+				return super.onConsoleMessage(consoleMessage);
+			}
+
+			@Nullable
+			@Override
+			public Bitmap getDefaultVideoPoster() {
+				return super.getDefaultVideoPoster();
+			}
+
+			@Nullable
+			@Override
+			public View getVideoLoadingProgressView() {
+				return super.getVideoLoadingProgressView();
+			}
+
+			@Override
+			public void getVisitedHistory(ValueCallback<String[]> callback) {
+				super.getVisitedHistory(callback);
+			}
+
+			@Override
+			public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+				return super.onShowFileChooser(webView, filePathCallback, fileChooserParams);
+			}
+		});
+
+		webview.addJavascriptInterface(new Object(){
+			@android.webkit.JavascriptInterface
+			public void callFromJs(final String str){
+				//通过js中的方法来控制android界面
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						Log.e("callfromjs",str);
+					}
+				});
+
+			}
+		},"zs");
+
+
+
+	}
+
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+//		判断当前按下的是返回键，并且webview可以返回，那就执行返回操作
+		if (keyCode==KeyEvent.KEYCODE_BACK&&webview.canGoBack()){
+				webview.goBack();
+				return  true;//如果不返回true，事件就无法起效
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 
 	public void OnClick(View view) {
-		url = et_url.getText().toString().trim();
-		if (TextUtils.isEmpty(url)) {
-			url = DEFAULT_URL;
-		}
-		/**加载地址*/
-		webview.loadUrl(url);
-		switch (view.getId()) {
-			case R.id.btn_go:
 
+		switch (view.getId()) {
+			case R.id.btn_go://加载网页地址
+				url = et_url.getText().toString().trim();
+				if (TextUtils.isEmpty(url)) {
+					url = DEFAULT_URL;
+				}
+				/**加载地址*/
+				webview.loadUrl(url);
+				break;
+			case R.id.btn_gethistory://获取网页历史记录
+				webBackForwardList = webview.copyBackForwardList();//获取历史记录
+				for (int i = 0; i < webBackForwardList.getSize(); i++) {
+					itemAtIndex = webBackForwardList.getItemAtIndex(i);//获取指定的历史记录
+					Log.e(getClass().getName().toString(),"---====url="+itemAtIndex.getUrl()+"---===originalurl"+itemAtIndex.getOriginalUrl()+"--====title"+itemAtIndex.getTitle());
+				}
+				break;
+			case R.id.btn_load:
+				//加载HTML中的js脚本
+				webview.loadUrl("javascript:testAlert();");
 				break;
 		}
 	}
